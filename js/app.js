028c2d818ec5209036e8cf40743e278a383eb5b1
Angel Martinez <angel.codes139@gmail.com>
2020-08-25 16:09:21 -0500

    clear the shopping cart

--- js/app.js
+++ js/app.js
@@ -8,89 +8,95 @@ let shoppingCartProducts = [];
 //* Event listeners
 loadEventListeners();
 function loadEventListeners() {
-	// When user click "Agregar al Carrito"
-	coursesList.addEventListener('click', addCourse);
+    // When user click "Agregar al Carrito"
+    coursesList.addEventListener('click', addCourse);
 
-	// Delete courses of the Shopping Cart
-	shoppingCart.addEventListener('click', removeCourse);
+    // Delete courses of the Shopping Cart
+    shoppingCart.addEventListener('click', removeCourse);
+    
+    // Clear the shopping cart
+    shoppingCarCleaner.addEventListener('click', () => {
+        shoppingCartProducts = []; // Clear the Array
+        clearHTML(); // Remove items from the shopping cart
+    });
 }
 
 //* Functions
 
 // Add new course in the Shopping Cart
 function addCourse(e) {
-	e.preventDefault(); // Prevent default action
+    e.preventDefault(); // Prevent default action
 
-	// Execute action only when the correct item is clicked
-	if (e.target.classList.contains('agregar-carrito')) {
-		readCourseData(e.target.parentElement.parentElement); // Selected course
-	}
+    // Execute action only when the correct item is clicked
+    if (e.target.classList.contains('agregar-carrito')) {
+        readCourseData(e.target.parentElement.parentElement); // Selected course
+    }
 }
 
 // Remove course from the shopping cart
 function removeCourse(e) {
-	// Execute action only when the correct item is clicked
-	if (e.target.classList.contains('borrar-curso')) {
-		// ID of the selected course
-		const idCourse = e.target.getAttribute('data-id');
+    // Execute action only when the correct item is clicked
+    if (e.target.classList.contains('borrar-curso')) {
+        // ID of the selected course
+        const idCourse = e.target.getAttribute('data-id');
 
-		// Remove from Array
-		shoppingCartProducts = shoppingCartProducts.filter(
-			(course) => course.id !== idCourse,
-		);
+        // Remove from Array
+        shoppingCartProducts = shoppingCartProducts.filter(
+            course => course.id !== idCourse
+        );
 
-		// Update the HTML
-		shoppingCartHTML();
-	}
+        // Update the HTML
+        shoppingCartHTML();
+    }
 }
 
 // Read the parent element of the clicked element
 function readCourseData(course) {
-	// Object with the course info
-	const courseInfo = {
-		id: course.querySelector('a').getAttribute('data-id'),
-		imagen: course.querySelector('img').src,
-		title: course.querySelector('h4').textContent,
-		price: course.querySelector('.precio span').textContent,
-		quantity: 1,
-	};
+    // Object with the course info
+    const courseInfo = {
+        id: course.querySelector('a').getAttribute('data-id'),
+        imagen: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.precio span').textContent,
+        quantity: 1,
+    };
 
-	// Check if a course already exists in the shopping cart
-	const exists = shoppingCartProducts.some(
-		(course) => course.id === courseInfo.id,
-	);
-	if (exists) {
-		// Update the quantity of the product
-		const courses = shoppingCartProducts.map((course) => {
-			if (course.id === courseInfo.id) {
-				course.quantity++;
-				return course;
-			} else {
-				return course;
-			}
-		});
-		shoppingCartProducts = [...courses];
-	} else {
-		// Add product to the Shopping Cart Products Array
-		shoppingCartProducts = [...shoppingCartProducts, courseInfo];
-	}
+    // Check if a course already exists in the shopping cart
+    const exists = shoppingCartProducts.some(
+        course => course.id === courseInfo.id
+    );
+    if (exists) {
+        // Update the quantity of the product
+        const courses = shoppingCartProducts.map((course) => {
+            if (course.id === courseInfo.id) {
+                course.quantity++;
+                return course;
+            } else {
+                return course;
+            }
+        });
+        shoppingCartProducts = [...courses];
+    } else {
+        // Add product to the Shopping Cart Products Array
+        shoppingCartProducts = [...shoppingCartProducts, courseInfo];
+    }
 
-	shoppingCartHTML();
+    shoppingCartHTML();
 }
 
 // Show shopping cart in HTML
 function shoppingCartHTML() {
-	// Clear HTML
-	clearHTML();
+    // Clear HTML
+    clearHTML();
 
-	// Generate the HTML for each Course
-	shoppingCartProducts.forEach((course) => {
-		// Extract values
-		const { id, imagen, title, price, quantity } = course;
+    // Generate the HTML for each Course
+    shoppingCartProducts.forEach((course) => {
+        // Extract values
+        const { id, imagen, title, price, quantity } = course;
 
-		// Create HTML template
-		const row = document.createElement('tr');
-		row.innerHTML = `
+        // Create HTML template
+        const row = document.createElement('tr');
+        row.innerHTML = `
             <td>
                 <img src="${imagen}" width="100">
             </td>
@@ -102,14 +108,14 @@ function shoppingCartHTML() {
             </td>
         `;
 
-		// Add HTML template in the tbody
-		shoppingCartContainer.appendChild(row);
-	});
+        // Add HTML template in the tbody
+        shoppingCartContainer.appendChild(row);
+    });
 }
 
 // Prevent duplicates courses
 function clearHTML() {
-	while (shoppingCartContainer.firstChild) {
-		shoppingCartContainer.removeChild(shoppingCartContainer.firstChild);
-	}
+    while (shoppingCartContainer.firstChild) {
+        shoppingCartContainer.removeChild(shoppingCartContainer.firstChild);
+    }
 }
